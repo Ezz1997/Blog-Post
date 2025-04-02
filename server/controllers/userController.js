6f1aabc8200a9e03f7d294315a05e58a187778f3
Ezz1997 <ezmarie97@gmail.com>
2025-04-02 14:33:55 +0300

    updated server to handle access and refresh tokens

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,21 @@
 import db from "../config/db_connection.js";
 import bcrypt from "bcryptjs";
 import { ObjectId } from "mongodb";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/generateToken.js";
 import jwt from "jsonwebtoken";
-
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
 // Route handler for GET /api/users
 // Get list of all users
 const getUsersHandler = async (req, res) => {
   const coll = db.collection("users");
-  const users = await coll.find().project({ password: 0 }).toArray();
+  const users = await coll
+    .find()
+    .project({ password: 0, refreshToken: 0 })
+    .toArray();
   // console.log(users);
 
   try {
@@ -128,6 +134,13 @@ const userLoginHandler = async (req, res) => {
 
   req.on("end", async () => {
     try {
+      // empty request body
+      if (!body) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ login: false, error: "Invalid request" }));
+        return;
+      }
+
       // Parse Json
       let user = JSON.parse(body);
       const { email, password } = user;
@@ -148,11 +161,32 @@ const userLoginHandler = async (req, res) => {
         const result = await bcrypt.compare(password, existingUser.password);
 
         if (result) {
-          const token = jwt.sign({ userId: existingUser._id }, JWT_SECRET, {
-            expiresIn: 60,
-          });
+          const accessToken = generateAccessToken(existingUser);
+          const refreshToken = generateRefreshToken(existingUser);
+
+          // save refresh token in db
+          const filter = { _id: existingUser._id };
+
+          // Specify the update to set a value for the plot field
+          const updateDoc = {
+            $set: {
+              refreshToken: refreshToken,
+            },
+          };
+
+          // update document with the specific id
+          const updated_res = await coll.updateOne(filter, updateDoc);
+
+          console.log(updated_res.modifiedCount);
+
           res.statusCode = 200;
-          res.end(JSON.stringify({ login: true, token: token }));
+          res.end(
+            JSON.stringify({
+              login: true,
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+            })
+          );
           return;
         }
       }
@@ -203,10 +237,176 @@ const deleteUserByIdHandler = async (req, res) => {
   }
 };
 
+const refreshToken = async (req, res) => {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", async () => {
+    try {
+      if (!body) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ error: "Invalid request" }));
+        return;
+      }
+
+      const data = JSON.parse(body);
+
+      // Take the refresh token from the user
+      const refreshToken = data.refreshToken;
+
+      // Token isn't in the request body
+      if (!refreshToken) {
+        res.statusCode = 401;
+        res.end(JSON.stringify({ error: "Not Authenticated" }));
+        return;
+      }
+
+      // Check if token is in db
+      const users = db.collection("users");
+      const user_with_token = await users.findOne({ refreshToken });
+
+      // Send error if token isn't in the db
+      if (!user_with_token) {
+        res.statusCode = 403;
+        res.end(JSON.stringify({ error: "Refresh token is not valid" }));
+        return;
+      }
+
+      jwt.verify(refreshToken, JWT_REFRESH_SECRET, async (err, user) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 401;
+
+          let errorMessage = "Unauthorized. Invalid token.";
+          if (err instanceof jwt.TokenExpiredError) {
+            errorMessage = "Unathorized. Token expired";
+          }
+          res.end(JSON.stringify({ error: errorMessage }));
+          return;
+        }
+
+        const newAccessToken = generateAccessToken(user);
+        const newRefreshToken = generateRefreshToken(user);
+
+        // Update refresh token in db
+        const filter = { _id: ObjectId.createFromHexString(user._id) };
+
+        // Specify the update to set a value for the plot field
+        const updateDoc = {
+          $set: {
+            refreshToken: newRefreshToken,
+          },
+        };
+
+        console.log(user._id);
+
+        // update document with the specific id
+        const updated_res = await users.updateOne(filter, updateDoc);
+
+        console.log(updated_res.modifiedCount);
+
+        res.statusCode = 200;
+        res.end(
+          JSON.stringify({
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken,
+          })
+        );
+      });
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end(JSON.stringify({ error: "Internal Server Error" }));
+    }
+  });
+};
+
+const logoutHandler = (req, res) => {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", async () => {
+    try {
+      if (!body) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ error: "Invalid request" }));
+        return;
+      }
+
+      const data = JSON.parse(body);
+
+      // Take the refresh token from the user
+      const refreshToken = data.refreshToken;
+
+      // Token isn't in the request body
+      if (!refreshToken) {
+        res.statusCode = 401;
+        res.end(JSON.stringify({ error: "Not Authenticated" }));
+        return;
+      }
+
+      // Check if token is in db
+      const users = db.collection("users");
+      const user_with_token = await users.findOne({ refreshToken });
+
+      // Send error if token isn't in the db
+      if (!user_with_token) {
+        res.statusCode = 403;
+        res.end(JSON.stringify({ error: "Refresh token is not valid" }));
+        return;
+      }
+
+      jwt.verify(refreshToken, JWT_REFRESH_SECRET, async (err, user) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 401;
+
+          let errorMessage = "Unauthorized. Invalid token.";
+          if (err instanceof jwt.TokenExpiredError) {
+            errorMessage = "Unathorized. Token expired";
+          }
+          res.end(JSON.stringify({ error: errorMessage }));
+          return;
+        }
+
+        // Update refresh token in db
+        const filter = { _id: ObjectId.createFromHexString(user._id) };
+
+        // Specify the update to set a value for the plot field
+        const updateDoc = {
+          $set: {
+            refreshToken: null,
+          },
+        };
+
+        // update document with the specific id
+        const updated_res = await users.updateOne(filter, updateDoc);
+
+        console.log(updated_res.modifiedCount);
+
+        res.statusCode = 200;
+        res.end(JSON.stringify({ message: "User logged out successfully!" }));
+      });
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end(JSON.stringify({ error: "Internal Server Error" }));
+    }
+  });
+};
+
 export {
   getUsersHandler,
   getUserByIdHandler,
   createUserHandler,
   userLoginHandler,
   deleteUserByIdHandler,
+  refreshToken,
+  logoutHandler,
 };
