9ee15b09dc727d3dcc5735545176180b5ea25727
Ezz1997 <ezmarie97@gmail.com>
2025-04-06 12:55:30 +0300

    updated the design

--- client/src/components/sign-in/SignIn.jsx
+++ client/src/components/sign-in/SignIn.jsx
@@ -1,178 +1,49 @@
-import * as React from "react";
 import {
+  Stack,
+  Typography,
+  TextField,
   Button,
-  FormControl,
+  ThemeProvider,
+  Link,
+  Alert,
+  CircularProgress,
+  Box,
   FormControlLabel,
   Checkbox,
-  InputLabel,
-  OutlinedInput,
-  TextField,
   InputAdornment,
-  Alert,
-  IconButton,
-  Link,
 } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { AppProvider } from "@toolpad/core/AppProvider";
-import { SignInPage } from "@toolpad/core/SignInPage";
-import { createTheme } from "@mui/material/styles";
-import { Link as RouterLink } from "react-router";
+import { createTheme } from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router";
+import { useState, useReducer, useEffect, useContext } from "react";
+import CustomPasswordField from "./CustomPasswordField";
+import { validateEmail, validatePassword } from "./ValidationFunctions";
 
-const providers = [{ id: "credentials", name: "Email and Password" }];
+import { INITIAL_STATE, postReducer } from "./postReducer";
+import { ACTION_TYPES } from "./postActionTypes";
+import { AppContext } from "../../context/AppContext";
 
-function CustomEmailField() {
-  return (
-    <TextField
-      id="input-with-icon-textfield"
-      label="Email"
-      name="email"
-      type="email"
-      size="small"
-      required
-      fullWidth
-      slotProps={{
-        input: {
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountCircle fontSize="inherit" />
-            </InputAdornment>
-          ),
-        },
-      }}
-      variant="outlined"
-    />
-  );
-}
+import { apiFetch } from "../../utils/interceptor";
 
-function CustomPasswordField() {
-  const [showPassword, setShowPassword] = React.useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  return (
-    <FormControl sx={{ my: 2 }} fullWidth variant="outlined">
-      <InputLabel size="small" htmlFor="outlined-adornment-password">
-        Password
-      </InputLabel>
-      <OutlinedInput
-        id="outlined-adornment-password"
-        type={showPassword ? "text" : "password"}
-        name="password"
-        size="small"
-        required
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              edge="end"
-              size="small"
-            >
-              {showPassword ? (
-                <VisibilityOff fontSize="inherit" />
-              ) : (
-                <Visibility fontSize="inherit" />
-              )}
-            </IconButton>
-          </InputAdornment>
-        }
-        label="Password"
-      />
-    </FormControl>
-  );
-}
-
-function CustomButton() {
-  return (
-    <Button
-      type="submit"
-      variant="outlined"
-      color="info"
-      size="small"
-      disableElevation
-      fullWidth
-      sx={{ my: 2 }}
-    >
-      Log In
-    </Button>
-  );
-}
-
-function SignUpLink() {
-  return (
-    <Link to="/signup" component={RouterLink}>
-      Sign up
-    </Link>
-  );
-}
-
-function ForgotPasswordLink() {
-  return (
-    <Link to="/forgot-password" component={RouterLink}>
-      Forgot password?
-    </Link>
-  );
-}
-
-function Title() {
-  return <h2 style={{ marginBottom: 8 }}>Login</h2>;
-}
-
-// function Subtitle() {
-//   return (
-//     <Alert sx={{ mb: 2, px: 1, py: 0.25 }} severity="warning">
-//       We are investigating an ongoing outage.
-//     </Alert>
-//   );
-// }
-
-function RememberMe() {
-  return (
-    <FormControlLabel
-      control={
-        <Checkbox
-          name="tandc"
-          value="true"
-          color="primary"
-          sx={{ padding: 0.5, "& .MuiSvgIcon-root": { fontSize: 20 } }}
-        />
-      }
-      slotProps={{
-        typography: {
-          fontSize: 14,
-        },
-      }}
-      color="textSecondary"
-      label="Remember me"
-    />
-  );
-}
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const PORT = import.meta.env.VITE_PORT;
 
 const theme = createTheme({
   palette: {
-    background: {
-      paper: "#222831",
-      default: "#eee",
-    },
     text: {
       primary: "#fff",
       secondary: "#fff",
     },
-    action: {
-      active: "#fff",
-    },
     primary: {
       main: "#F6B17A",
     },
-    info: {
-      main: "#F6B17A",
+    action: {
+      active: "#fff",
+    },
+  },
+  typography: {
+    h5: {
+      fontWeight: 600, // or 'bold'
     },
   },
   components: {
@@ -194,62 +65,200 @@ const theme = createTheme({
   },
 });
 
-export default function SignIn() {
-  const handleSubmit = (data) => {
-    console.log("Email: ", data.get("email"));
-    console.log("Password: ", data.get("password"));
+function RememberMe() {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          name="tandc"
+          value="true"
+          color="primary"
+          sx={{ padding: 0.5, "& .MuiSvgIcon-root": { fontSize: 20 } }}
+        />
+      }
+      slotProps={{
+        typography: {
+          fontSize: 14,
+          color: "textPrimary",
+        },
+      }}
+      label="Remember me"
+    />
+  );
+}
 
-    fetch("http://localhost:8000/api/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: data.get("email"),
-        password: data.get("password"),
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+function SignIn() {
+  const [message, setMessage] = useState("");
+  const [state, dispatch] = useReducer(postReducer, INITIAL_STATE);
+
+  const { setAccessToken } = useContext(AppContext);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [dataValidity, setDataValidity] = useState({
+    email: true,
+    password: true,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    let isValid = true;
+
+    if (name === "email") {
+      isValid = validateEmail(value);
+    } else if (name === "password") {
+      isValid = validatePassword(value);
+    }
+
+    setDataValidity({ ...dataValidity, [name]: isValid });
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return;
+    }
+
+    for (let name in dataValidity) {
+      if (!dataValidity[name]) {
+        return;
+      }
+    }
+
+    dispatch({ type: ACTION_TYPES.POST_START });
+
+    try {
+      const res = await apiFetch(`${BASE_URL}:${PORT}/api/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password,
+        }),
+        credentials: "include",
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw data.error || "";
+      }
+
+      if (data.message === "Login successful" && data.accessToken) {
+        console.log(data);
+        setAccessToken(data.accessToken);
+        dispatch({ type: ACTION_TYPES.POST_SUCCESS });
+        navigate("/");
+      }
+    } catch (err) {
+      setMessage(typeof err === "string" ? err : "");
+      dispatch({ type: ACTION_TYPES.POST_ERROR });
+
+      console.error(err);
+    }
   };
 
   return (
-    <AppProvider theme={theme}>
-      <SignInPage
-        signIn={
-          (provider, formData) => handleSubmit(formData)
-          // alert(
-          //   `Logging in with "${provider.name}" and credentials: ${formData.get(
-          //     "email"
-          //   )}, ${formData.get("password")}, and checkbox value: ${formData.get(
-          //     "tandc"
-          //   )}`
-          // )
-        }
-        slots={{
-          title: Title,
-          // subtitle: Subtitle,
-          emailField: CustomEmailField,
-          passwordField: CustomPasswordField,
-          submitButton: CustomButton,
-          signUpLink: SignUpLink,
-          rememberMe: RememberMe,
-          forgotPasswordLink: ForgotPasswordLink,
-        }}
-        providers={providers}
-        slotProps={{
-          form: {
-            autoComplete: "off",
-            sx: {
-              height: "250px",
-            },
-          },
-        }}
-      />
-    </AppProvider>
+    <ThemeProvider theme={theme}>
+      <Stack>
+        <Box
+          sx={{
+            display: "grid",
+            placeItems: "center",
+            width: "100%",
+            minHeight: "100vh", // Ensures content never overflows
+          }}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              p: 2,
+              mt: 2,
+              mb: 2,
+              width: { xs: "70%", sm: "350px", md: "370px", xl: "25vw" },
+              minHeight: "380px",
+              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px;",
+              borderRadius: "1%",
+              backgroundColor: "#222831",
+            }}
+            component="form"
+            autoComplete="off"
+            alignItems="center"
+            justifyContent="center"
+            onSubmit={handleSubmit}
+          >
+            <Typography color="textPrimary" variant="h5" align="center">
+              Login
+            </Typography>
+            <TextField
+              error={!dataValidity["email"]}
+              id="email"
+              label="Email"
+              helperText={dataValidity["email"] ? "" : "Invalid email"}
+              sx={{ width: "90%" }}
+              size="small"
+              name="email"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle fontSize="inherit" />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <CustomPasswordField
+              value={formData.password}
+              onChange={handleChange}
+              customWidth={"90%"}
+              error={!dataValidity["password"]}
+              helperText={
+                dataValidity["password"]
+                  ? ""
+                  : "Password must include upper/lowercase letters, numbers & special characters"
+              }
+            />
+            <Stack
+              direction="row"
+              sx={{ width: "85%" }}
+              justifyContent="space-between"
+            >
+              <RememberMe />
+              <Link to="/forgot-password" component={RouterLink} align="right">
+                Forgot password?
+              </Link>
+            </Stack>
+            <Button type="submit" variant="outlined" sx={{ width: "90%" }}>
+              LOG IN
+            </Button>
+            <Link to="/signup" component={RouterLink}>
+              Sign up
+            </Link>
+            {state.loading && <CircularProgress />}
+            {state.error && (
+              <Alert severity="error">
+                {message || "Something went wrong"}
+              </Alert>
+            )}
+          </Stack>
+        </Box>
+      </Stack>
+    </ThemeProvider>
   );
 }
+
+export default SignIn;
