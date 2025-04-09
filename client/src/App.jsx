fcda57a7702ca6ce4a7062313e13be2beafc3f7d
Ezz1997 <ezmarie97@gmail.com>
2025-04-09 22:57:33 +0300

    refactored the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,10 +31,11 @@ const Error = lazy(() => import("./components/Error.jsx"));
 function MainLayout() {
   const location = useLocation();
 
-  // Check if current route is new-post
-  const isNewPostPage = location.pathname === "/new-post";
+  let mainClassName =
+    location.pathname === "/new-post" ? "grid-container" : "flex-container";
 
   const { isLoading } = useContext(AppContext);
+
   return (
     <>
       <Header />
@@ -51,54 +52,27 @@ function MainLayout() {
           <CircularProgress />
         </div>
       ) : (
-        !isNewPostPage && (
-          <main className="flex-container">
-            <Routes>
-              {/* Protected Routes - User needs to be logged in*/}
-              <Route element={<ProtectedRoute />}>
-                <Route index element={<Homepage />} />
-                <Route path="/blog-post" element={<BlogPost />} />
-                <Route path="/test" element={<Test />} />
-              </Route>
+        <main className={mainClassName}>
+          <Routes>
+            {/* Protected Routes - User needs to be logged in*/}
+            <Route element={<ProtectedRoute />}>
+              <Route index element={<Homepage />} />
+              <Route path="/blog-post" element={<BlogPost />} />
+              <Route path="/new-post" element={<NewPost />} />
+              <Route path="/test" element={<Test />} />
+            </Route>
 
-              {/* Public Routes - User not logged in only*/}
-              <Route element={<PublicRoute />}>
-                <Route path="/login" element={<SignIn />} />
-              </Route>
+            {/* Public Routes - User not logged in only*/}
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<SignIn />} />
+            </Route>
 
-              {/* Global Routes - Accessable regardless of if the user is logged in or not*/}
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="*" element={<Error />} />
-            </Routes>
-          </main>
-        )
-      )}
-
-      {/* This component works with lexical and it can't be 
-      nested in a flex-container */}
-      {isLoading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <CircularProgress />
-        </div>
-      ) : (
-        isNewPostPage && (
-          <main className="grid-container">
-            <Routes>
-              {/* Public Routes - User not logged in only*/}
-              <Route element={<ProtectedRoute />}>
-                <Route path="/new-post" element={<NewPost />} />
-              </Route>
-            </Routes>
-          </main>
-        )
+            {/* Global Routes - Accessable regardless of if the user is logged in or not*/}
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
       )}
 
       <Footer />
