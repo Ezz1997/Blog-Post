bc6dc528baa1c8c7b0111dfe02e4a20a5e7816c2
Ezz1997 <ezmarie97@gmail.com>
2025-06-12 10:25:41 +0300

    replaced hardcoded values with constant variables

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,14 +4,17 @@ import "dotenv/config";
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
+const ACCESS_TOKEN_EXPIRATION = 60 * 15;
+const REFRESH_TOKEN_EXPIRATION = 60 * 60 * 24;
+
 export const generateAccessToken = (user) => {
   return jwt.sign({ _id: user._id }, JWT_ACCESS_SECRET, {
-    expiresIn: 60 * 15, // 15 minutes
+    expiresIn: ACCESS_TOKEN_EXPIRATION, // 15 minutes
   });
 };
 
 export const generateRefreshToken = (user) => {
   return jwt.sign({ _id: user._id }, JWT_REFRESH_SECRET, {
-    expiresIn: 60 * 60 * 24, // 24 hours
+    expiresIn: REFRESH_TOKEN_EXPIRATION, // 24 hours
   });
 };
